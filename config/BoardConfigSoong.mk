b70083d8af3859519a61fc5b8f9e43eba50ced6a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-26 21:02:01 +0530

    soong: Add support for miui camera mode [2/2]
    Change-Id: I0c912a1c7056bb204a24bd5f0a08308cae71f3f3

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -44,7 +44,8 @@ SOONG_CONFIG_genesisGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
-    uses_egl_display_array
+    uses_egl_display_array \
+    uses_miui_camera
 
 SOONG_CONFIG_NAMESPACES += genesisNvidiaVars
 SOONG_CONFIG_genesisNvidiaVars += \
@@ -73,6 +74,7 @@ SOONG_CONFIG_genesisNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWOR
 SOONG_CONFIG_genesisQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_genesisQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_genesisQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_genesisGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
